e709345bd8baa61b6a62d6d4ee9eba4ecb9d2236
Sarai Misidjan <Sarai@conduction.nl>
2022-12-22 11:04:12 +0100

    add object if attribute has a reference

--- Subscriber/ObjectReferenceSubscriber.php
+++ Subscriber/ObjectReferenceSubscriber.php
@@ -7,17 +7,22 @@ namespace CommonGateway\CoreBundle\Subscriber;
 use App\Entity\Attribute;
 use App\Entity\Entity;
 use CommonGateway\CoreBundle\Service\EavService;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Cache\Adapter\AdapterInterface as CacheInterface;
 
 class ObjectReferenceSubscriber implements EventSubscriberInterface
 {
     private EavService $eavService;
+    private EntityManagerInterface $entityManager;
 
     public function __construct(
+        EntityManagerInterface $entityManager,
         EavService $eavService
     ) {
+        $this->entityManager = $entityManager;
         $this->eavService = $eavService;
     }
 
@@ -26,15 +31,14 @@ class ObjectReferenceSubscriber implements EventSubscriberInterface
     public function getSubscribedEvents(): array
     {
         return [
-            Events::prePersist,
+            Events::prePersist
         ];
     }
 
     /**
-     * Checks wheter we should check atributes and entities for connections.
+     * Checks whether we should check attributes and entities for connections
      *
      * @param LifecycleEventArgs $args
-     *
      * @return void
      */
     public function prePersist(LifecycleEventArgs $args): void
@@ -43,20 +47,23 @@ class ObjectReferenceSubscriber implements EventSubscriberInterface
 
         // Let see if we need to hook an attribute to an entity
         if (
-            $object instanceof Attribute // Its an atribute
-            && $object->getReference() // It has an reference
-            && !$object->getObject() // It isnt currently connected to a schema
+            $object instanceof Attribute // It's an attribute
+            && $object->getReference() // It has a reference
+            && !$object->getObject() // It isn't currently connected to a schema
         ) {
-            //$this->cacheService->cacheObject($object);
+            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $object->getReference()]);
+            $object->setObject($entity);
+//            $this->cacheService->cacheObject($object);
             return;
         }
         if (
             $object instanceof Entity // Is it an antity
-            && $object->getReference() // Does it have an reference
+            && $object->getReference() // Does it have a reference
         ) {
 
             //$this->cacheService->cacheShema($object);
             return;
         }
+        return;
     }
 }
