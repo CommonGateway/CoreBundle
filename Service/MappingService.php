f06582ca5456756e4adf8d9ed7a0e51b3aa312ed
Sarai Misidjan <Sarai@conduction.nl>
2023-01-26 10:42:51 +0100

    fix mappingService

--- Service/MappingService.php
+++ Service/MappingService.php
@@ -5,60 +5,100 @@ namespace CommonGateway\CoreBundle\Service;
 use Adbar\Dot;
 use App\Entity\Mapping;
 use Doctrine\ORM\EntityManagerInterface;
+use Monolog\Logger;
+use Twig\Environment;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+/**
+ * The mapping service handles the mapping (or transformation) of array A (input) to array B (output)
+ *
+ * More information on how to write your own mappings can be found at [/docs/mapping.md](/docs/mapping.md).
+ */
 class MappingService
 {
-    private EntityManagerInterface $em;
+    // Add symfony style bundle in order to output to the console.
+    private SymfonyStyle $io;
 
+    // Create a private variable to store the twig environment
+    private Environment $twig;
+
+    /**
+     * Setting up the base class with required services
+     *
+     * @param Environment $twig
+     */
     public function __construct(
-        EntityManagerInterface $em
+        Environment $twig
     ) {
-        $this->em = $em;
+        $this->twig = $twig;
     }
 
     /**
-     * Set symfony style in order to output to the console
+     * Set symfony style in order to output to the console.
      *
      * @param SymfonyStyle $io
+     *
      * @return self
      */
     public function setStyle(SymfonyStyle $io): self
     {
         $this->io = $io;
-        $this->synchronizationService->setStyle($io);
 
         return $this;
     }
 
+    /**
+     * Maps (transforms) an array (input) to a different array (output)
+     *
+     * @param Mapping $mappingObject The mapping object that forms the recipe for the mapping
+     * @param array $input The array that need to be mapped (transformed) otherwise known as input
+     * @return array The result (output) of the mapping process
+     */
     public function mapping(Mapping $mappingObject, array $input): array
     {
-        $this->io->debug("Mapping array based on mapping object ".$mappingObject->getName()." (id:".$mappingObject->getId()->toString()." / ref:".$mappingObject->getReference().") v:".$mappingObject->getversion());
+            isset($this->io) ?? $this->io->debug("Mapping array based on mapping object ".$mappingObject->getName()." (id:".$mappingObject->getId()->toString()." / ref:".$mappingObject->getReference().") v:".$mappingObject->getversion());
 
-        // Determine pass trough
+        $output = [];
+
+        // Check for throughput
         if($mappingObject->getPassTrough()){
-            $dot = new Dot($input);
-            $this->io->debug("Mapping *with* pass trough");
-        }
-        else{
-            $dot = new Dot();
-            $this->io->debug("Mapping *without* pass trough");
+            $output = $input;
+                isset($this->io) ?? $this->io->debug("Mapping *with* pass trough");
+        } else {
+                isset($this->io) ?? $this->io->debug("Mapping *without* pass trough");
         }
 
-        // Lets do actual mapping
-        foreach ($mappingObject->getMapping() as $key => $value){
-            $dot->set($key, $value); //todo need to be twig
+        // Let's get the dot array based on https://github.com/adbario/php-dot-notation
+        $dotArray = dot($output);
+
+        // Let's do the actual mapping
+        foreach($mappingObject->getMapping() as $key => $value){
+            // Render the value from twig
+            $dotArray->set($key, $this->twig->createTemplate($value)->render($input));
         }
 
-        // Casting
-        foreach ($mappingObject->getCast() as $key => $type){
+        // Unset unwanted key's
+        foreach ($mappingObject->getUnset() as $unset){
+            if(!$dotArray->has($unset)){
+                    isset($this->io) ?? $this->io->debug("Trying to unset an property that doesn't exist during mapping");
+                continue;
+            }
+            $dotArray->delete($unset);
+        }
 
-            $value = $dot->get($key);
+        // Cast values to a specific type
+        foreach ($mappingObject->getCast() as $key => $cast){
+            if(!$dotArray->has($key)){
+                    isset($this->io) ?? $this->io->debug("Trying to cast an property that doesn't exist during mapping");
+                continue;
+            }
 
-            switch ($type) {
-                case "int":
-                case "integer":
-                    echo "i equals 0";
+            $value = $dotArray->get($key);
+
+            switch ($cast) {
+                case 'int':
+                case 'integer':
+                    $value = intval($value);
                     break;
                 case "bool":
                 case "boolean":
@@ -67,19 +107,26 @@ class MappingService
                 case "string":
                     echo "i equals 2";
                     break;
+                // Todo: Add more casts
                 default:
+                        isset($this->io) ?? $this->io->debug("Trying to cast to an unsupported cast type: ".$cast);
                     break;
             }
 
-            $dot->set($key, $value);
+            $dotArray->set($key, $value);
         }
 
-        // Unset
-        foreach ($mappingObject->getUnset() as $value){
-            $dot->delete($value);
-        }
+        // Back to array
+        $output = $dotArray->all();
 
-        // Lets return the result
-        return $requestMapping;
+        // Log the result
+            isset($this->io) ?? $this->io->debug('Mapped object',[
+            "input" => $input,
+            "output" => $output,
+            "passTrough" => $mappingObject->getPassTrough(),
+            "mapping" => $mappingObject->getMapping(),
+        ]);
+
+        return $output;
     }
 }
