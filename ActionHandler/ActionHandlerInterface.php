7328c0e6c9530e61600762686e8f1cbc4c1c0695
Ruben van der Linde <ruben@conduction.nl>
2023-02-05 21:03:19 +0100

    Docblocks for the interface

--- ActionHandler/ActionHandlerInterface.php
+++ ActionHandler/ActionHandlerInterface.php
@@ -7,13 +7,17 @@ namespace CommonGateway\CoreBundle\ActionHandler;
  */
 interface ActionHandlerInterface
 {
-    /*
+    /**
      * All action handles should be able to return there configuration requirements and options as a (json-schema)[https://json-schema.org/understanding-json-schema/reference/object.html] object
      */
     public function getConfiguration();
 
-    /*
+    /**
      * All action handlers should implement the run function that is called when the action is triggerd
+     * 
+     * @param array $data
+     * @param array $configuration
+     * @return mixed
      */
     public function run(array $data, array $configuration);
 }
