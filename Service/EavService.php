1934117240c5b3a5d3a74881065c23765b7fd9fb
Ruben van der Linde <ruben@conduction.nl>
2023-02-27 08:51:03 +0100

    Hot Fix for deleting entities

--- Service/EavService.php
+++ Service/EavService.php
@@ -77,16 +77,16 @@ class EavService
      * @param Entity|null $entity An optionall entity to remove all the objects from
      * @return bool True is succesfull or false otherwise
      */
-    public function deleteAllObjects(?Entity $entity):bool
+    public function deleteAllObjects(Entity $entity):bool
     {
         // Get al the objects for a specifi entity
         if(isset($entity) === true){
-            $objects = $this->entityManager->getRepository('App:ObjecyEntity')->findBy(['entity'=>$entity]);
+            $objects = $this->entityManager->getRepository('App:ObjectEntity')->findBy(['entity'=>$entity]);
         }
 
         // Or just get all the objects
         if(isset($entity) === false){
-            $objects = $this->entityManager->getRepository('App:ObjecyEntity')->findAll();
+            //$objects = $this->entityManager->getRepository('App:ObjectEntity')->findAll();
         }
 
         // Annnnnnd lets delete them
