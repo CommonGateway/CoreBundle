a5e268e49b0561608bdd3b6f64eeced9f868f4c0
Sarai Misidjan <Sarai@conduction.nl>
2023-03-02 15:39:35 +0100

    updated OasService

--- Service/OasService.php
+++ Service/OasService.php
@@ -26,8 +26,9 @@ class OasService
      */
     public function __construct(
         EntityManagerInterface $entityManager,
-        ParameterBagInterface $parameters
-    ) {
+        ParameterBagInterface  $parameters
+    )
+    {
         $this->entityManager = $entityManager;
         $this->parameters = $parameters;
     }//end __construct()
@@ -43,21 +44,21 @@ class OasService
         // Setup the basic oas array.
         $oas = [
             'openapi' => '3.0.0',
-            'info'    => [
-                'title'      => 'Common Gateway',
-                'description'=> 'The Common Gateway is a further Dutch development of the European API Platform. API Platform is a project of Les Tilleus and, in itself, an extension of the Symfony framework. API Platform is a tool for delivering APIs based on standardized documentation and is used for various French and German government projects. Including Digital state, a precursor to Xroute, GOV.UK and Common Ground. The project is now part of joinup.eu (the European equivalent of Common Ground).',
-                'version'    => '1.0.3',
+            'info' => [
+                'title' => 'Common Gateway',
+                'description' => 'The Common Gateway is a further Dutch development of the European API Platform. API Platform is a project of Les Tilleus and, in itself, an extension of the Symfony framework. API Platform is a tool for delivering APIs based on standardized documentation and is used for various French and German government projects. Including Digital state, a precursor to Xroute, GOV.UK and Common Ground. The project is now part of joinup.eu (the European equivalent of Common Ground).',
+                'version' => '1.0.3',
             ],
             'servers' => [
                 [
-                'url'         => $this->parameters->get('app_url', 'https://localhost'),
-                'description' => 'The kubernetes server',
+                    'url' => $this->parameters->get('app_url', 'https://localhost'),
+                    'description' => 'The kubernetes server',
                 ]
             ],
-            'paths'      => [],
+            'paths' => [],
             'components' => [],
-            'security'   => [],
-            'tags'       => [],
+            'security' => [],
+            'tags' => [],
         ];
 
         // Add the endpoints.
@@ -85,7 +86,15 @@ class OasService
         // Add the endpoints to the OAS.
         foreach ($endpoints as $endpoint) {
             // Add the path to the paths.
-            $oas['paths']['/'.implode('/', $endpoint->getPath())] = $this->getEndpointOperations($endpoint);
+            $pathArray = [];
+            foreach ($endpoint->getPath() as $path) {
+                if ($path === 'id') {
+                    $pathArray[] = '{' . $path . '}';
+                    continue;
+                }
+                $pathArray[] = $path;
+            }
+            $oas['paths']['/' . implode('/', $pathArray)] = $this->getEndpointOperations($endpoint);
 
             // Add the schemas.
             $oas['components']['schemas'] = array_merge($oas['components']['schemas'], $this->getEndpointSchemas($endpoint));
@@ -108,40 +117,89 @@ class OasService
         // Lets take a look at the methods.
         foreach ($endpoint->getMethods() as $method) {
             // We dont do a request body on GET, DELETE and UPDATE requests.
-            if (in_array($method, ['DELETE', 'UPDATE']) === true) {
-                $operations[$method] = [
-                    'summary'     => $endpoint->getName(),
+            if (in_array($method, ['DELETE']) === true) {
+                $operations[strtolower($method)] = [
+                    'operationId' => strtolower($endpoint->getName() . '-' . $method),
+                    'summary' => $endpoint->getName(),
+                    'tags' => [strtolower($endpoint->getName())],
                     'description' => $endpoint->getDescription(),
+                    'responses' => [
+                        '200' => [
+                            'description' => $endpoint->getDescription(),
+                            'content' => [
+                                'application/json' => [
+                                    'schema' => [
+                                        'type' => 'string',
+                                        'example' => 'Object is successfully deleted'
+                                    ]
+                                ],
+                                'application/xml' => [
+                                    'schema' => [
+                                        'type' => 'string',
+                                        'example' => 'Object is successfully deleted'
+                                    ]
+                                ],
+                            ],
+                        ],
+                    ],
                 ];
                 continue;
             }
 
             // In all other cases we want include a schema.
-            $operations[$method] = [
-                'summary'     => $endpoint->getName(),
+            $operations[strtolower($method)] = [
+                'operationId' => strtolower($endpoint->getName() . '-' . $method),
+                'summary' => $endpoint->getName(),
+                'tags' => [strtolower($endpoint->getName())],
                 'description' => $endpoint->getDescription(),
+                'parameters' => [
+                    ['name' => 'id',
+                        'in' => 'path',
+                        'description' => '',
+                        'required' => true,
+                        'schema' => [
+                            'type' => 'string'
+                        ]
+                    ]
+                ],
                 'requestBody' => [
                     'description' => $endpoint->getDescription(),
                     //'required' =>// Todo: figure out what we want to do here
                     'content' => [
-                        'application/json' => '#/components/schemas/'.$endpoint->getEntities()->first()->getName(),
-                        'application/xml'  => '#/components/schemas/'.$endpoint->getEntities()->first()->getName(),
+                        'application/json' => [
+                            'schema' => [
+                                '$ref' => '#/components/schemas/' . $endpoint->getEntities()->first()->getName()
+                            ],
+                        ],
+                        'application/xml' => [
+                            'schema' => [
+                                '$ref' => '#/components/schemas/' . $endpoint->getEntities()->first()->getName()
+                            ],
+                        ]
                     ],
                 ],
                 'responses' => [
                     '200' => [
                         'description' => $endpoint->getDescription(),
-                        'content'     => [
-                            'application/json' => '#/components/schemas/'.$endpoint->getEntities()->first()->getName(),
-                            'application/xml'  => '#/components/schemas/'.$endpoint->getEntities()->first()->getName(),
+                        'content' => [
+                            'application/json' => [
+                                'schema' => [
+                                    '$ref' => '#/components/schemas/' . $endpoint->getEntities()->first()->getName()
+                                ]
+                            ],
+                            'application/xml' => [
+                                'schema' => [
+                                    '$ref' => '#/components/schemas/' . $endpoint->getEntities()->first()->getName()
+                                ]
+                            ],
                         ],
                     ],
                 ],
             ];
 
-            // If we are dealing with a get request we do not expect an requestBody.
-            if ($method === 'GET') {
-                unset($operations[$method]['requestBody']);
+            // We dont do a request body on GET, DELETE and UPDATE requests.
+            if ($method === 'GET' || $method === 'PUT') {
+                unset($operations[strtolower($method)]['requestBody']);
             }
 
             // TODO: Collection endpoints
