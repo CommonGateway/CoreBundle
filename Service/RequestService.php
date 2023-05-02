c57644fc6aa886e287acb5c61e6385adb5ace5d8
Robert Zondervan <robert@conduction.nl>
2023-05-02 11:49:00 +0200

    Style fixes

--- Service/RequestService.php
+++ Service/RequestService.php
@@ -376,8 +376,8 @@ class RequestService
             //            }
             $filters = $this->realRequestQueryAll($this->data['method']);
 
-            foreach($filters as $key=>$value) {
-                if($value === 'all' || $value === 'alles' || $value === '*') {
+            foreach ($filters as $key=>$value) {
+                if ($value === 'all' || $value === 'alles' || $value === '*') {
                     unset($filters[$key]);
                 }
             }
