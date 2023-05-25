730617c2571eeba3d3a6dc25a4d1f8ba0d7799a1
Robert Zondervan <robert@conduction.nl>
2023-05-25 14:42:11 +0200

    Reset weird alignments

--- Service/MappingService.php
+++ Service/MappingService.php
@@ -116,16 +116,16 @@ class MappingService
 
         $input = $this->encodeArrayKeys($input, '.', '&#46;');
 
-            isset($this->io) ?? $this->io->debug('Mapping array based on mapping object '.$mappingObject->getName().' (id:'.$mappingObject->getId()->toString().' / ref:'.$mappingObject->getReference().') v:'.$mappingObject->getversion());
+        isset($this->io) ?? $this->io->debug('Mapping array based on mapping object '.$mappingObject->getName().' (id:'.$mappingObject->getId()->toString().' / ref:'.$mappingObject->getReference().') v:'.$mappingObject->getversion());
 
         // Determine pass trough
         // Let's get the dot array based on https://github.com/adbario/php-dot-notation
         if ($mappingObject->getPassTrough()) {
             $dotArray = new Dot($input);
-                isset($this->io) ?? $this->io->debug('Mapping *with* pass trough');
+            isset($this->io) ?? $this->io->debug('Mapping *with* pass trough');
         } else {
             $dotArray = new Dot();
-                isset($this->io) ?? $this->io->debug('Mapping *without* pass trough');
+            isset($this->io) ?? $this->io->debug('Mapping *without* pass trough');
         }
 
         $dotInput = new Dot($input);
@@ -145,7 +145,7 @@ class MappingService
         // Unset unwanted key's
         foreach ($mappingObject->getUnset() as $unset) {
             if (!$dotArray->has($unset)) {
-                    isset($this->io) ?? $this->io->debug("Trying to unset an property that doesn't exist during mapping");
+                isset($this->io) ?? $this->io->debug("Trying to unset an property that doesn't exist during mapping");
                 continue;
             }
             $dotArray->delete($unset);
@@ -154,7 +154,7 @@ class MappingService
         // Cast values to a specific type
         foreach ($mappingObject->getCast() as $key => $cast) {
             if (!$dotArray->has($key)) {
-                    isset($this->io) ?? $this->io->debug("Trying to cast an property that doesn't exist during mapping");
+                isset($this->io) ?? $this->io->debug("Trying to cast an property that doesn't exist during mapping");
                 continue;
             }
 
@@ -180,7 +180,7 @@ class MappingService
         }
 
         // Log the result
-            isset($this->io) ?? $this->io->debug('Mapped object', [
+        isset($this->io) ?? $this->io->debug('Mapped object', [
             'input'      => $input,
             'output'     => $output,
             'passTrough' => $mappingObject->getPassTrough(),
@@ -247,7 +247,7 @@ class MappingService
                 $value = $this->coordinateStringToArray($value);
                 break;
             default:
-                    isset($this->io) ?? $this->io->debug('Trying to cast to an unsupported cast type: '.$cast);
+                isset($this->io) ?? $this->io->debug('Trying to cast to an unsupported cast type: '.$cast);
                 break;
         }
 
