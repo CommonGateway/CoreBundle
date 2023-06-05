44795a70e2ea3e381c60cda52767992207e8b352
Robert Zondervan <robert@conduction.nl>
2023-06-05 13:39:37 +0200

    Small fix to use ID instead of reference for finding templates

--- Service/DownloadService.php
+++ Service/DownloadService.php
@@ -25,7 +25,8 @@ class DownloadService
 
     public function render(array $data): string
     {
-        $criteria = Criteria::create()->where(Criteria::expr()->memberOf("supportedSchemas", $data['_self']['schema']['ref']));
+        var_dump($data['_self']['schema']['id']);
+        $criteria = Criteria::create()->where(Criteria::expr()->memberOf("supportedSchemas", $data['_self']['schema']['id']));
 
         $templates = new ArrayCollection($this->entityManager->getRepository('App:Template')->findAll());
         $templates = $templates->matching($criteria);
