fa839d241000bcc805a4300d283db677cb0269c1
Wilco Louwerse <wilco@conduction.nl>
2023-06-06 16:35:38 +0200

    Some code cleanup

--- Service/NotificationService.php
+++ Service/NotificationService.php
@@ -3,7 +3,11 @@
 namespace CommonGateway\CoreBundle\Service;
 
 use Adbar\Dot;
+use App\Entity\Gateway as Source;
+use App\Entity\Synchronization;
 use App\Service\SynchronizationService;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,6 +34,11 @@ class NotificationService
     private array $data;
     
     /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+    
+    /**
      * @var LoggerInterface
      */
     private LoggerInterface $logger;
@@ -45,15 +54,18 @@ class NotificationService
     private GatewayResourceService $gatewayResourceService;
 
     /**
+     * @param EntityManagerInterface $entityManager          The EntityManager.
      * @param LoggerInterface        $notificationLogger     The notification logger.
      * @param SynchronizationService $synchronizationService The SynchronizationService.
      * @param GatewayResourceService $gatewayResourceService The GatewayResourceService.
      */
     public function __construct(
+        EntityManagerInterface $entityManager,
         LoggerInterface $notificationLogger,
         SynchronizationService $synchronizationService,
         GatewayResourceService $gatewayResourceService
     ) {
+        $this->entityManager = $entityManager;
         $this->logger = $notificationLogger;
         $this->synchronizationService = $synchronizationService;
         $this->gatewayResourceService = $gatewayResourceService;
@@ -78,38 +90,81 @@ class NotificationService
     
         $this->logger->debug("NotificationService -> notificationHandler()");
         
-        // Find source by resource url from the notification
-        $dot = new Dot($data);
-        $url = $dot->get($configuration['urlLocation']);
-        $sources = $this->gatewayResourceService->findSourcesForUrl($url, 'commongateway/corebundle');
-        if (count($sources) === 0) {
-            $response = ["Message" => "Could not find a Source with this url: $url"];
-            return ["response" => new Response(json_encode($response), 400, ['Content-type' => 'application/json'])];
-        }
-        if (count($sources) > 1) {
-            $response = ["Message" => "Found more than one Source (".count($sources).") with this url: $url"];
-            // todo: maybe we want to just use the first one found or the one that matches the most, or just repeat for all sources?
-            return ["response" => new Response(json_encode($response), 400, ['Content-type' => 'application/json'])];
-        }
-        $source = $sources[0];
-        
-        // Get the correct Entity
-        $entity = $this->gatewayResourceService->getSchema($configuration['entity'], 'commongateway/corebundle');
-        if ($entity === null) {
-            $response = ["Message" => "Could not find an Entity with this reference: {$configuration['entity']}"];
-            return ["response" => new Response(json_encode($response), 500, ['Content-type' => 'application/json'])];
+        // Find Synchronization with the data from the notification and action->configuration.
+        try {
+            $synchronization = $this->findSync();
+        } catch (Exception $exception) {
+            $this->logger->error($exception->getMessage());
+            
+            $response = json_encode(["Message" => $exception->getMessage()]);
+            
+            return ["response" => new Response($response, $exception->getCode(), ['Content-type' => 'application/json'])];
         }
         
-        // Get (source) id from notification data
-        $explodedUrl = explode('/', $url);
-        $id = end($explodedUrl);
-        
-        $synchronization = $this->synchronizationService->findSyncBySource($source, $entity, $id);
         $this->synchronizationService->synchronize($synchronization);
+        
+        $this->entityManager->flush();
         
         $response = ["Message" => "Notification received, object synchronized"];
         $data['response'] = new Response(json_encode($response), 200, ['Content-type' => 'application/json']);
 
         return $data;
     }//end notificationHandler()
+    
+    /**
+     * Tries to find a synchronisation with the data from the notification and action->configuration.
+     *
+     * @return Synchronization
+     *
+     * @throws Exception If we could not find a Source or Entity we throw an exception.
+     */
+    private function findSync(): Synchronization
+    {
+        $dot = new Dot($this->data);
+        $url = $dot->get($this->configuration['urlLocation']);
+    
+        // Find source by resource url from the notification
+        $source = $this->findSource($url);
+    
+        // Get the correct Entity
+        $entity = $this->gatewayResourceService->getSchema($this->configuration['entity'], 'commongateway/corebundle');
+        if ($entity === null) {
+            throw new Exception("Could not find an Entity with this reference: {$this->configuration['entity']}", 500);
+        }
+    
+        // Get (source) id from notification data
+        $explodedUrl = explode('/', $url);
+        $id = end($explodedUrl);
+    
+        $synchronization = $this->synchronizationService->findSyncBySource($source, $entity, $id);
+        $synchronization->setEndpoint(str_replace($source->getLocation(), '', $url));
+        $this->entityManager->persist($synchronization);
+        
+        return $synchronization;
+    }
+    
+    /**
+     * Tries to find a source using the url of the object a notification was created for.
+     *
+     * @param string $url The url we are trying to find a matching Source for.
+     *
+     * @return Source The Source we found.
+     *
+     * @throws Exception If we did not find one Source we throw an exception.
+     */
+    private function findSource(string $url): Source
+    {
+        $sources = $this->gatewayResourceService->findSourcesForUrl($url, 'commongateway/corebundle');
+        
+        if (count($sources) === 0) {
+            throw new Exception("Could not find a Source with this url: $url", 400);
+        }
+        
+        if (count($sources) > 1) {
+            // todo: maybe we want to just use the first one found or the one that matches the most, or just repeat for all sources?
+            throw new Exception("Found more than one Source (".count($sources).") with this url: $url", 400);
+        }
+        
+        return $sources[0];
+    }//end findSource()
 }
