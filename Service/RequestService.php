88a032bf60089bb58764cfce2d55e3667c023925
Robert Zondervan <robert@conduction.nl>
2023-06-06 09:36:09 +0200

    small revert of a file that should not have been committed

--- Service/RequestService.php
+++ Service/RequestService.php
@@ -110,7 +110,6 @@ class RequestService
 
         $accept   = $this->data['accept'];
 
-        $endpoint = null;
         if (isset($this->data['endpoint']) === true) {
             $endpoint = $this->data['endpoint'];
         }
