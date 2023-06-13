7e40839a1cfdd39f0a39c06bad985834a2893273
Wilco Louwerse <wilco@conduction.nl>
2023-06-13 10:36:05 +0200

    more cleanup

--- Subscriber/ObjectUriSubscriber.php
+++ Subscriber/ObjectUriSubscriber.php
@@ -11,36 +11,62 @@ use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
+/**
+ * Todo: ???
+ *
+ * @Author Conduction <info@conduction.nl>
+ *
+ * @license EUPL <https://github.com/ConductionNL/contactcatalogus/blob/master/LICENSE.md>
+ *
+ * @category Subscriber
+ */
 class ObjectUriSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var ParameterBagInterface
+     */
     private ParameterBagInterface $parameterBag;
+    
+    /**
+     * @var SessionInterface
+     */
     private SessionInterface $session;
-
+    
+    /**
+     * The constructor sets al needed variables.
+     *
+     * @param ParameterBagInterface $parameterBag
+     * @param SessionInterface $session
+     */
     public function __construct(
         ParameterBagInterface $parameterBag,
         SessionInterface $session
     ) {
         $this->parameterBag = $parameterBag;
         $this->session = $session;
-    }
-
-    // this method can only return the event names; you cannot define a
-    // custom method name to execute when each event triggers
+    }//end __construct()
+    
+    /**
+     * Todo: ???
+     *
+     * @return array This method can only return the event names;
+     * You cannot define a custom method name to execute when each event triggers.
+     */
     public function getSubscribedEvents(): array
     {
         return [
             Events::postPersist,
             Events::postUpdate,
         ];
-    }
+    }//end getSubscribedEvents()
 
     public function postUpdate(LifecycleEventArgs $args): void
     {
         $this->postPersist($args);
-    }
+    }//end postUpdate
 
     /**
-     * Updates the chache whenever an object is put into the database.
+     * Updates the cache whenever an object is put into the database.
      *
      * @param LifecycleEventArgs $args
      *
@@ -57,5 +83,5 @@ class ObjectUriSubscriber implements EventSubscriberInterface
 
             return;
         }
-    }
-}
+    }//end postPersist()
+}//end class
