b74d5c4617aba6949034e8889ffc8917f227b0e8
Wilco Louwerse <wilco@conduction.nl>
2023-06-16 10:08:49 +0200

    Fix for finding services.yaml

--- src/DependencyInjection/CoreExtension.php
+++ src/DependencyInjection/CoreExtension.php
@@ -9,11 +9,15 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class CoreExtension extends Extension
 {
-
-
+    /**
+     * @param array            $configs   The config
+     * @param ContainerBuilder $container The container builder
+     *
+     * @return void
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
         $loader->load('services.yaml');
 
     }//end load()
