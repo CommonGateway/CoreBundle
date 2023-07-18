fa1a7480f82449f9f4f74aa82f89c48366986add
Sarai Misidjan <Sarai@conduction.nl>
2023-07-18 17:15:59 +0200

    revert commit

--- src/Service/ValidationService.php
+++ src/Service/ValidationService.php
@@ -615,6 +615,8 @@ class ValidationService
         case 'dutch_pc4':
             return new CustomRules\DutchPostalcode();
         case 'date':
+        case 'date-time':
+        case 'datetime':
             // For now...
         case 'duration':
             // For now...
