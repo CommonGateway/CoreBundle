29ff08fdb399fb1be2b0a7a3c9fe591d9945ac01
Robert Zondervan <robert@conduction.nl>
2023-08-09 16:21:15 +0200

    Cleanup and support for various Excel formats

--- src/Service/UploadService.php
+++ src/Service/UploadService.php
@@ -14,10 +14,13 @@ namespace CommonGateway\CoreBundle\Service;
 use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\YamlEncoder;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
+use Symfony\Component\Serializer\Serializer;
 use function Safe\json_decode;
 
 class UploadService
@@ -28,59 +31,86 @@ class UploadService
      */
     private array $supportedFileExtensions = [
         'xlsx',
+        'xls',
+        'ods',
         'json',
         'csv',
         'xml',
         'yaml',
     ];
 
-    public function upload(Request $request): string
+    /**
+     * Decodes an incoming file based upon its extension.
+     *
+     * @param string       $extension The extension of the file to decode.
+     * @param UploadedFile $file      The uploaded file to decode.
+     * @param Request      $request   The incoming request.
+     *
+     * @return array The array of objects derived from the file.
+     */
+    public function decodeFile(string $extension, UploadedFile $file, Request $request): array
     {
-        // Unsure about what the standard name will be here.
-        $file = $request->files->get('upload');
-        if ($file instanceof UploadedFile === false) {
-            return new Exception("No file uploaded.");
-        }
-
-        // Validate file extension.
-        $extension = $file->getClientOriginalExtension();
-        if (in_array($extension, $this->supportedFileExtensions) === false) {
-            return new Exception("File extension: $extension not supported.");
-        }
-
         // Get the file content as a string.
         $fileContent = file_get_contents($file->getPathname());
 
+        // Create a serializer for the most common formats.
+        $serializer = new Serializer([],
+            [
+                new CsvEncoder([
+                    'no_headers'    => $request->request->get('headers') === 'true' ? false : true,
+                    'csv_delimiter' => $request->request->has('delimiter') ? $request->request->get('delimiter') : ',',
+                ]),
+                new YamlEncoder(),
+                new JsonEncoder(),
+                new XmlEncoder()
+            ]
+        );
+
         switch ($extension) {
-        case 'xlsx':
-            // Load the XLSX file using PhpSpreadsheet
-            $spreadsheet = IOFactory::load($file->getPathname());
+            case 'xlsx':
+            case 'ods':
+            case 'xls':
+                // Load the XLSX file using PhpSpreadsheet
+                $spreadsheet = IOFactory::load($file->getPathname());
 
-            // Convert the XLSX data to an array
-            $worksheet = $spreadsheet->getActiveSheet();
-            $data      = $worksheet->toArray();
-            break;
-        case 'json':
-            $data = json_decode($fileContent);
-            break;
-        case 'csv':
-            $data = str_getcsv($fileContent);
-            break;
-        case 'xml':
-            $xmlEncoder = new XmlEncoder();
-            $data       = $xmlEncoder->decode($fileContent, 'xml');
-            break;
-        case 'yaml':
-            $yamlEncoder = new YamlEncoder();
-            $data        = $yamlEncoder->decode($fileContent, 'yaml');
-            break;
+                // Convert the XLSX data to an array
+                $worksheet = $spreadsheet->getActiveSheet();
+                $data      = $worksheet->toArray();
+
+                if($request->request->get('headers') === 'true'){
+                    $headers = array_shift($data);
+                    $data = $this->toKeyedRows($data, $headers);
+                }
+
+                $data['objects'] = $data;
+
+                break;
+            default:
+                $data = $serializer->decode($fileContent, $extension);
         }//end switch
-
-        var_dump($data);
 
         $objects = $data['objects'];
 
         return $objects;
 
+    }//end decodeFile()
+
+    public function upload(Request $request): array
+    {
+        // Unsure about what the standard name will be here.
+        $file = $request->files->get('upload');
+        if ($file instanceof UploadedFile === false) {
+            throw new Exception("No file uploaded.");
+        }
+
+        // Validate file extension.
+        $extension = $file->getClientOriginalExtension();
+        if (in_array($extension, $this->supportedFileExtensions) === false) {
+            throw new Exception("File extension: $extension not supported.");
+        }
+
+        return $this->decodeFile($extension, $file, $request);
+
     }//end upload()
+
 }//end class
