1f63529a5bb6d2e917f0ec53b878d7c2fe9fcffc
Wilco Louwerse <wilco@conduction.nl>
2023-08-10 13:39:03 +0200

    Updated HydrationService

--- src/Service/HydrationService.php
+++ src/Service/HydrationService.php
@@ -6,6 +6,7 @@ use App\Entity\Attribute;
 use App\Entity\Entity;
 use App\Entity\Gateway as Source;
 use App\Entity\ObjectEntity;
+use App\Entity\Synchronization;
 use App\Service\SynchronizationService;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -51,16 +52,18 @@ class HydrationService
     /**
      * Recursively loop through an object, check if a synchronisation exists or create one (if necessary).
      *
-     * @param array  $object        The object array ready for hydration.
-     * @param Source $source        The source the objects need to be connected to.
-     * @param Entity $entity        The entity of the (sub)object.
-     * @param bool   $unsafeHydrate If we should hydrate unsafely or not (when true it will unset non given properties).
+     * @param array  $object                The object array ready for hydration.
+     * @param Source $source                The source the objects need to be connected to.
+     * @param Entity $entity                The entity of the (sub)object.
+     * @param bool   $unsafeHydrate         If we should hydrate unsafely or not (when true it will unset non given properties).
+     * @param bool   $returnSynchronization If we should return the Synchronization of the main object instead of the ObjectEntity/array.
      *
-     * @return array|ObjectEntity The resulting object or array.
+     * @return array|ObjectEntity|Synchronization The resulting object, Synchronization or array.
      */
-    public function searchAndReplaceSynchronizations(array $object, Source $source, Entity $entity, bool $flush = true, bool $unsafeHydrate = false)
+    public function searchAndReplaceSynchronizations(array $object, Source $source, Entity $entity, bool $flush = true, bool $unsafeHydrate = false, bool $returnSynchronization = false)
     {
         foreach ($object as $key => $value) {
+            // Check if we are dealing with an array of subobjects.
             if (is_array($value) === true) {
                 $subEntity = $entity;
                 if ($entity->getAttributeByName($key) !== false
@@ -71,11 +74,29 @@ class HydrationService
                 }
 
                 $object[$key] = $this->searchAndReplaceSynchronizations($value, $source, $subEntity, $flush);
-            } else if ($key === '_sourceId') {
+                
+                continue;
+            }
+            
+            // If we are dealing with the $key _sourceId prioritise it over the $key = 'id'
+            if ($key === '_sourceId') {
+                // If the value in _sourceId = null we can't create a Synchronization for it.
+                if ($value === null) {
+                    return []; // todo: ?
+                }
+                $synchronization = $this->syncService->findSyncBySource($source, $entity, $value);
+                unset($object['_sourceId']);
+                
+                continue;
+            }
+            
+            // By default, if we find an id field we use that to create a synchronization.
+            if (($key === 'id' || $key === '_id') && isset($synchronization) === false) {
                 $synchronization = $this->syncService->findSyncBySource($source, $entity, $value);
             }
         }
 
+        // Todo: here we want to do the default syncToGateway synchronization, without creating extra/duplicate objects though...
         if (isset($synchronization) === true) {
             if ($synchronization->getObject() instanceof ObjectEntity === false) {
                 $synchronization->setObject(new ObjectEntity($entity));
@@ -89,7 +110,10 @@ class HydrationService
                 $this->entityManager->flush();
                 $this->entityManager->flush();
             }
-
+            
+            if ($returnSynchronization === true) {
+                return $synchronization;
+            }
             return $synchronization->getObject();
         }
 
