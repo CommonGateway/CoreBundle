dbab64a48e9c63bcef7b14e383126b496b930b24
Wilco Louwerse <wilco@conduction.nl>
2023-08-17 13:18:36 +0200

    Make sure we do not have an array of accept headers

--- src/Service/CallService.php
+++ src/Service/CallService.php
@@ -256,6 +256,10 @@ class CallService
         if (isset($overwriteContentType) === true) {
             $config['headers']['Content-Type'] = $overwriteContentType;
         }
+        // Make sure we do not have an array of accept headers
+        if (isset($config['headers']['accept']) === true && is_array($config['headers']['accept']) === true) {
+            $config['headers']['accept'] = $config['headers']['accept'][0];
+        }
 
         $config['headers']['host'] = $parsedUrl['host'];
         $config['headers']         = $this->removeEmptyHeaders($config['headers']);
