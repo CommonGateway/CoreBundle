71a6e6b9edfa68e7106a49a4a5d1e2c6bb0d6770
Robert Zondervan <robert@conduction.nl>
2023-09-27 14:51:54 +0200

    Fixes from copy errors

--- src/Service/CallService.php
+++ src/Service/CallService.php
@@ -619,7 +619,14 @@ class CallService
         }
 
         // This if is statement prevents binary code from being used a string.
-        if ($contentType !== 'application/pdf') {
+        if (in_array($contentType,
+                [
+                    'application/pdf',
+                    'application/pdf; charset=utf-8',
+                    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                    'application/vnd.openxmlformats-officedocument.wordprocessingml.document; charset=utf-8'
+                ]
+            ) === false) {
             $this->callLogger->debug('Response content: '.$responseBody);
         }
 
@@ -632,23 +639,25 @@ class CallService
         }
 
         switch ($contentType) {
-        case 'text/yaml':
-        case 'text/x-yaml':
-        case 'text/yaml; charset=utf-8':
-            return $yamlEncoder->decode($responseBody, 'yaml');
-        case 'text/xml':
-        case 'text/xml; charset=utf-8':
-        case 'application/pdf':
-        case 'application/pdf; charset=utf-8':
-            $this->callLogger->debug('Response content: pdf binary code..');
-            return ['base64' => base64_encode($responseBody)];
-        case 'application/xml':
-        case 'application/xml; charset=utf-8':
-            return $xmlEncoder->decode($responseBody, 'xml');
-        case 'application/json':
-        case 'application/json; charset=utf-8':
-        default:
-            $result = json_decode($responseBody, true);
+            case 'text/yaml':
+            case 'text/x-yaml':
+            case 'text/yaml; charset=utf-8':
+                return $yamlEncoder->decode($responseBody, 'yaml');
+            case 'text/xml':
+            case 'text/xml; charset=utf-8':
+            case 'application/pdf':
+            case 'application/pdf; charset=utf-8':
+            case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+            case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document; charset=utf-8':
+                $this->callLogger->debug('Response content: binary code..');
+                return ['base64' => base64_encode($responseBody)];
+            case 'application/xml':
+            case 'application/xml; charset=utf-8':
+                return $xmlEncoder->decode($responseBody, 'xml');
+            case 'application/json':
+            case 'application/json; charset=utf-8':
+            default:
+                $result = json_decode($responseBody, true);
         }//end switch
 
         if (isset($result) === true) {
