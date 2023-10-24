dfb1b89abcd6d4180b04c51bc96b54e1526f4d7c
Wilco Louwerse <wilco@conduction.nl>
2023-10-24 09:47:09 +0200

    update test ActionEvents

--- src/Service/CallService.php
+++ src/Service/CallService.php
@@ -317,11 +317,15 @@ class CallService
                 'commongateway.action.event',
                 [
                     'log_level' => 'notice',
-                    'source'    => [
+                    'source' => [
                         'id'        => $source->getId(),
                         'reference' => $source->getReference(),
                     ],
-                    'response'  => ['name' => 'test'],
+                    'response' => [
+                        'name' => 'test',
+                        'code' => 200,
+                        'message' => "$method Request to $url returned {$response->getStatusCode()}"
+                    ]
                 ],
                 'commongateway.monolog.create'
             );
@@ -337,11 +341,15 @@ class CallService
                 'commongateway.action.event',
                 [
                     'log_level' => 'error',
-                    'source'    => [
+                    'source' => [
                         'id'        => $source->getId(),
                         'reference' => $source->getReference(),
                     ],
-                    'response'  => ['name' => 'test'],
+                    'response' => [
+                        'name' => 'test',
+                        'code' => $exception->getCode(),
+                        'message' => 'Request failed with error '.$exception
+                    ]
                 ],
                 'commongateway.monolog.create'
             );
@@ -361,11 +369,15 @@ class CallService
                 'commongateway.action.event',
                 [
                     'log_level' => 'error',
-                    'source'    => [
+                    'source' => [
                         'id'        => $source->getId(),
                         'reference' => $source->getReference(),
                     ],
-                    'response'  => ['name' => 'test'],
+                    'response' => [
+                        'name' => 'test',
+                        'code' => $exception->getCode(),
+                        'message' => 'Request failed with error '.$exception
+                    ]
                 ],
                 'commongateway.monolog.create'
             );
