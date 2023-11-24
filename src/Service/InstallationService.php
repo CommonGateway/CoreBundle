ec852afe01efbb774fb771ed915788e0fff79c60
Wilco Louwerse <wilco@conduction.nl>
2023-11-24 11:30:32 +0100

    Let's prevent the creation of broken DashboardCards

--- src/Service/InstallationService.php
+++ src/Service/InstallationService.php
@@ -1837,6 +1837,12 @@ class InstallationService
                     $this->logger->error('No object found for '.$reference.' while trying to create a DashboardCard.');
                     continue;
                 }
+                
+                $class = get_class($object);
+                if (str_starts_with('App\Entity', $class) === false) {
+                    $this->logger->error('Couldn\'t get the class of '.$object->getId().' while trying to create a DashboardCard');
+                    continue;
+                }
 
                 // Check if this dashboardCard already exists.
                 $dashboardCard = $this->entityManager->getRepository('App:DashboardCard')->findOneBy(['entity' => get_class($object), 'entityId' => $object->getId()]);
