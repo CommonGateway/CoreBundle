a0dceefb69d2c760e67bdae0619bac3280e6d4da
Robert Zondervan <robert@conduction.nl>
2024-05-01 16:09:45 +0200

    Add dynamic CORS header

--- src/Subscriber/ResponseSubscriber.php
+++ src/Subscriber/ResponseSubscriber.php
@@ -2,7 +2,10 @@
 
 namespace CommonGateway\CoreBundle\Subscriber;
 
+use App\Entity\Application;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
@@ -11,24 +14,23 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class ResponseSubscriber implements EventSubscriberInterface
 {
 
-    /**
-     * @var EntityManagerInterface
-     */
-    private EntityManagerInterface $entityManager;
-
-    /**
-     * @var SessionInterface
-     */
-    private SessionInterface $session;
+    private LoggerInterface $logger;
 
     /**
      * @param EntityManagerInterface $entityManager The entity manager
      * @param SessionInterface       $session       The sesion interface
      */
-    public function __construct(EntityManagerInterface $entityManager, SessionInterface $session)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly SessionInterface $session,
+        private readonly ParameterBagInterface $parameterBag,
+        LoggerInterface $applicationLogger
+
+    )
     {
-        $this->entityManager = $entityManager;
-        $this->session       = $session;
+//        $this->entityManager = $entityManager;
+//        $this->session       = $session;
+        $this->logger = $applicationLogger;
 
     }//end __construct()
 
@@ -58,7 +60,19 @@ class ResponseSubscriber implements EventSubscriberInterface
             ]
         );
 
-        $response->headers->remove('Access-Control-Allow-Origin');
+        if($this->session->has('application')) {
+            $application = $this->entityManager->getRepository(Application::class)->find($this->session->get('application'));
+
+            $origin = $event->getRequest()->headers->get('origin');
+
+            $allowedOrigins = array_merge($application->getOrigins(), $this->parameterBag->get('cors_origins'));
+            if(in_array(needle: $origin, haystack: $allowedOrigins) || $application->getOrigins() === []) {
+                if($application->getOrigins() === []) {
+                    $this->logger->warning('Deprecated: No origins set for application, in the future, this will result in CORS blocking');
+                }
+                $response->headers->set('Access-Control-Allow-Origin', $origin);
+            }
+        }
 
     }//end request()
 }//end class
